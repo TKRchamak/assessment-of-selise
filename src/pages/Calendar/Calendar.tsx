630722cb9d4254a9901e6f4273e256a8cb4344ab
Tonmoy kumar Roy <tonmoykumarroy6@gmail.com>
2023-10-22 13:44:17 +0600

    add calender and some function in this page

--- src/pages/Calendar/Calendar.tsx
+++ src/pages/Calendar/Calendar.tsx
@@ -1,14 +1,102 @@
 import { useParams } from "react-router-dom";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import { useEffect, useState } from "react";
 
 const Calendar = () => {
   const { year, month } = useParams();
+  const yearList = ["2019", "2020", "2021"];
+  const monthList = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+  ];
+  const [selectedYear, setSelectedYear] = useState<string>(
+    `${new Date().getFullYear()}`
+  );
+  const [selectedMonth, setSelectedMonth] = useState<string>(
+    `${new Date().getMonth()}`
+  );
+
+  useEffect(() => {
+    if (!year) return;
+
+    setSelectedYear(year);
+    if (month) {
+      setSelectedMonth(month);
+    }
+  }, [year, month]);
 
   return (
-    <div>
-      {year} {month}
-      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+    <div className="p-5">
+      <div className="mb-2 flex justify-between">
+        <div className="flex">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn m-1 w-48">
+              {selectedYear}
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {yearList?.map((item) => (
+                <li
+                  key={item}
+                  onClick={() => {
+                    setSelectedYear(item);
+                  }}
+                >
+                  <a className={item === selectedYear ? "text-red-600" : ""}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="dropdown">
+            <label tabIndex={1} className="btn m-1 w-48">
+              {selectedMonth}
+            </label>
+            <ul
+              tabIndex={1}
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {monthList?.map((item) => (
+                <li
+                  key={item}
+                  onClick={() => {
+                    setSelectedMonth(item);
+                  }}
+                >
+                  <a className={item === selectedMonth ? "text-red-600" : ""}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          {/* <button>Create Appointment</button> */}
+          <button className="btn m-1">Create Appointment</button>
+        </div>
+      </div>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        // header={false}
+        initialView="dayGridMonth"
+      />
     </div>
   );
 };
